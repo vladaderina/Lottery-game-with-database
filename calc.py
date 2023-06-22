c88cb60a2fc54b315fa2d0d3d9580a84eb6e684e
vladaderina <96880544+vladaderina@users.noreply.github.com>
2023-06-23 03:14:52 +0500

    Create .gitignore

---
+++ calc.py
@@ -0,0 +1,22 @@
+import re
+def calc(expression):
+    reg = r'[+\-*/]'
+    if not re.search(reg, expression):
+        raise ValueError('Выражение должно содержать оператор')
+    sign = re.findall(reg,expression)[0]
+    try:
+        first, second = expression.split(sign)
+        first, second = int(first), int(second)
+        operators = {
+            '+': lambda a, b: a + b,
+            '-': lambda a, b: a - b,
+            '*': lambda a, b: a * b,
+            '/': lambda a, b: a / b
+        }
+        return operators[sign](first, second)
+    except (ValueError, TypeError):
+        raise ValueError('Выражение должно содержать только 2 целых числа и 1 знак')
+
+if __name__ == '__main__':
+    exp = input('Введи выражение типа a ? b на месте ? может стоять любой оперенд \n')
+    print(calc(exp))
