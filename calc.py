16029805b3d33c21e1bb69e41e905f0f6bc27f92
vladaderina <96880544+vladaderina@users.noreply.github.com>
2023-06-23 03:28:01 +0500

    Update calc.cpython-311.pyc

--- calc.py
+++ calc.py
@@ -7,13 +7,15 @@ def calc(expression):
     try:
         first, second = expression.split(sign)
         first, second = int(first), int(second)
-        operators = {
-            '+': lambda a, b: a + b,
-            '-': lambda a, b: a - b,
-            '*': lambda a, b: a * b,
-            '/': lambda a, b: a / b
-        }
-        return operators[sign](first, second)
+        match (sign):
+            case("+"):
+                return first + second
+            case ("-"):
+                return first - second
+            case ("/"):
+                return first / second
+            case ("*"):
+                return first * second
     except (ValueError, TypeError):
         raise ValueError('Выражение должно содержать только 2 целых числа и 1 знак')
 
